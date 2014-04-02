0daf65e94f39d007d19bf72c4062c9a18d80b834
Erik Aybar <erikthedeveloper@gmail.com>
2014-04-02 00:04:34 -0600

    Added connect-proxy and middleware for livereload. Targeting localhost:3000 for /api URI calls for backend server

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('swFrontApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $resource) {
+    $resource('/api/edges').query();
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
